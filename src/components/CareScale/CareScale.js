b9be55023360343e2a112163703644534d10501b
Jelena Bojovic <jelenabojovic981@gmail.com>
2023-10-05 14:37:43 +0200

    bug resolved

--- src/components/CareScale/CareScale.js
+++ src/components/CareScale/CareScale.js
@@ -1,29 +1,31 @@
-import React from 'react';
+import React from "react";
 import  { ReactComponent as Sun } from '../../assets/images/sun.svg';
-import  { ReactComponent as Water} from '../../assets/images/water.svg';
+import  { ReactComponent as Water } from '../../assets/images/water.svg';
 
-function infosCare (type, value) {
+function infosCare(type, value) {
     let quantity;
 
-    switch (value) {
-        case 1: quantity = "peu";
-             break;
-        case 2: quantity = "modérément";
-             break;
-        case 3: quantity = "beaucoup";
-             break;
+    switch(value) {
+        case 1 : quantity = 'peu';
+            break;
+        case 2 : quantity = 'modérément';
+            break;
+        case 3 : quantity = 'beaucoup';
+            break;
         default: quantity = '';
     }
+
     let cat = type === 'water' ? "d'arrosage" : "de lumière";
 
     alert(`Cette plante requière ${quantity} ${cat}`);
+
 }
 
-export default function infosCare ({scaleValue, careType}) {
+export default function CareScale({scaleValue, careType}) {
     const range = [1,2,3];
 
     const scaleType = careType === 'light' ? <Sun /> : <Water />;
-
+    
     return (
         <div onClick={() => infosCare(careType, scaleValue)}>{
             range.map(element => 
@@ -32,5 +34,3 @@ export default function infosCare ({scaleValue, careType}) {
         }</div>
     )
 }
-
-
